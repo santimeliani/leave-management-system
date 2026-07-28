85bea004735f1390c4e2e08aa344077b9b892859
santimeliani <santimeliani413@gmail.com>
2026-07-28 23:55:30 +0700

    fix: trust all proxies to fix HTTPS URL generation behind Railway load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
-})
+    ->withMiddleware(function (Middleware $middleware) {
+     $middleware->alias([
+         'role' => \App\Http\Middleware\RoleMiddleware::class,
+     ]);
+     $middleware->trustProxies(at: '*');
+ })
     
     ->withExceptions(function (Exceptions $exceptions): void {
         //
